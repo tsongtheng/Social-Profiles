65d4faa8378ae669b9c4ea97e9f1441d8a0844f6
Tipchan Songtheng <tipchans777@gmail.com>
2023-10-05 03:43:38 +0530

    add footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -3,7 +3,11 @@ import React from "react";
 import "./Footer.css";
 
 const Footer = () => {
-  return <div className="app__footer bg__footer section__padding">Footer</div>;
+  return (
+    <div className="app__footer bg__footer section__padding">
+      © 2023 Tipchan Songtheng
+    </div>
+  );
 };
 
 export default Footer;
