c36a040f63007c1b068a8f1cb3c4ab1bc32c3a7a
Tipchan Songtheng <tipchans777@gmail.com>
2023-10-06 01:12:41 +0530

    create Profiles & add content section

--- src/Container/Content/Content.jsx
+++ src/Container/Content/Content.jsx
@@ -1,9 +1,19 @@
 import React from "react";
+import { v4 as uuidv4 } from "uuid";
+
 import Card from "../../components/Card/Card";
+import Profiles from "../../Profile/Profiles";
+
+import "./Content.css";
 
 const Content = () => {
+  console.log(Profiles[0].imageLink);
   return (
-    <div className="app__content bg__content section__padding">content</div>
+    <div className="app__content section__padding">
+      {Profiles.map((obj, key) => (
+        <Card key={uuidv4()} obj={obj} />
+      ))}
+    </div>
   );
 };
 
---
+++ src/Profiles/Profiles.js
@@ -0,0 +1,13 @@
+let Profiles = [
+  {
+    name: "Tipchan Sontheng",
+    imageLink: "https://avatars.githubusercontent.com/u/69814398?v=4",
+    twitterLink: "https://twitter.com/tipchan_s",
+    facebookLink: "",
+    githubLink: "https://github.com/tsongtheng",
+    linkedinLink: "https://www.linkedin.com/in/tipchan-songtheng-5417991a8/",
+    instagramLink: "",
+  },
+];
+
+export default Profiles;
