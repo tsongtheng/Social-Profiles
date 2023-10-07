900929e2a3718e274b8d15d0e6ea87e0bce8268e
Jagpreet Singh Khurana <119691733+Jagpreet153@users.noreply.github.com>
2023-10-07 15:56:23 +0530

    Added Profile

--- src/Profiles/Profiles.js
+++ src/Profiles/Profiles.js
@@ -54,6 +54,16 @@ let Profiles = [
    linkedinLink: "",
    instagramLink: "",
  },
+
+ {
+  name:"Jagpreet Singh Khurana",
+  imageLink:"",
+  twitterLink: "",
+   facebookLink: "",
+   githubLink: "https://github.com/Jagpreet153",
+   linkedinLink: "",
+   instagramLink: "",
+ }
 ];
 
 export default Profiles;
